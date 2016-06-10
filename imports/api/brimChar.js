a82540fcbcd8c7c4e36d5a6f503c974a093548cf
Spencer Judd <spencerjfam@hotmail.com>
2016-06-10 16:00:31 -0600

    added slot items
    added two more menus

--- imports/api/brimChar.js
+++ imports/api/brimChar.js
@@ -2,5 +2,6 @@ import { Mongo } from 'meteor/mongo';
 
 export const Characters = new Mongo.Collection('characters');
 export const inventorydb = new Mongo.Collection('inventory');
+export const slotsdb = new Mongo.Collection('slots');
 export const sidebagdb = new Mongo.Collection('sidebag');
 export const extrasdb = new Mongo.Collection('extras');
